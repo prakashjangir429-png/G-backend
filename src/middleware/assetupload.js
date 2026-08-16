e7aca4c16e985f3664c5632c5b6cd6e7158f16a1
prakashjangir429-png <prakashjangir429@gmail.com>
2026-08-16 13:51:15 +0530

    dd

--- src/middleware/assetupload.js
+++ src/middleware/assetupload.js
@@ -4,9 +4,9 @@ import fs from "fs";
 
 const uploadDir = "uploads";
 
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
+// if (!fs.existsSync(uploadDir)) {
+//     fs.mkdirSync(uploadDir, { recursive: true });
+// }
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
